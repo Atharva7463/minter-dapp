f55c7dcbe85f8732acfd695674b6d3e4e1292d6e
Atharva Pandav <90105982+Atharva7463@users.noreply.github.com>
2024-04-14 22:49:49 +0530

    removed address from sepolia ntw app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -158,7 +158,7 @@ async function checkChain() {
                 chainName: 'Sepolia Test Network',
                 chainId: '11155111', // Replace '0xsepoliachainid' with the actual chain ID of Sepolia
                 nativeCurrency: { name: '	SepoliaETH', decimals: 18, symbol: 'SEPT' }, // Adjust currency details as per Sepolia's native currency
-                rpcUrls: ['https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'], // Replace 'your-infura-project-id' with your actual Infura project ID for Sepolia
+                rpcUrls: ['https://sepolia.infura.io/v3/'], // Replace 'your-infura-project-id' with your actual Infura project ID for Sepolia
             },
         ],
     });
