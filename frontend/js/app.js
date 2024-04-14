bf1bba53749e024de2afff647872433053af0548
Atharva Pandav <90105982+Atharva7463@users.noreply.github.com>
2024-04-14 23:05:48 +0530

    Update d symbol app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -131,7 +131,7 @@ async function checkChain() {
                 {
                     chainName: 'Sepolia Test Network',
                     chainId: web3.utils.toHex(chainId), // Replace '0xsepoliachainid' with the actual chain ID of Sepolia
-                    nativeCurrency: { name: 'SEPT', decimals: 18, symbol: 'SepoliaETH' }, // Adjust currency details as per Sepolia's native currency
+                    nativeCurrency: { name: 'SEPT', decimals: 18, symbol: 'ETH' }, // Adjust currency details as per Sepolia's native currency
                     rpcUrls: ['https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'], // Replace 'your-infura-project-id' with your actual Infura project ID for Sepolia
                 },
             ],
